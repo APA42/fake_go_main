b8019c5507e0454c360cdfdfb7213d27af02a245
Alberto Perez <aperez@alea-soluciones.com>
2018-01-23 14:58:32 +0100

    Use fake_go_component_1 at main

--- fake_go_main.go
+++ fake_go_main.go
@@ -1,7 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/APA42/fake_go_component_1"
+)
 
 func main(){
 	fmt.Println("Fake Main Compoment -> Executable")
+
+	var firstFakeComponent = fake_go_component_1.FakeComponentStruct{}
+
+	fmt.Println(firstFakeComponent.DoSomething())
 }
